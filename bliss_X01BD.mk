ed7225e461b670be87078de5734ae1bf97022cf9
niteshwani <niteshwani2@gmail.com>
2020-05-16 06:40:00 +0000

    X01BD:update build desc

--- bliss_X01BD.mk
+++ bliss_X01BD.mk
@@ -46,7 +46,7 @@ TARGET_VENDOR := asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X01BD_1 \
     PRODUCT_NAME=WW_X01BD \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1684 release-keys"
 
 # Build fingerprint
 BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
